e06bf2cce3b0225dd61cc63f0b4bfd628b6dffbb
Mahi130594 <enugurthymahendra@gmail.com>
2025-02-05 13:08:05 +0530

    Eligibility check code added

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 name = input("Enter the name: ")
 print(name)
+
+age = int(input("ENter the age: "))
+
+print(age)
+if age >= 18:
+    print("Eligible for Vote")
+else:
+    print("NOt Eligible for Vote")
